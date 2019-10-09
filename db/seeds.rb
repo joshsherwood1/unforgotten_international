e05b24d4ad967d52208780a7223cbecab9a9be0a
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-10-08 23:25:45 -0600

    Update seed file with two people objects

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Person.destroy_all
+Country.destroy_all
+@sherif = Person.create!(name: "Sherif Gaber", picture: "https://pbs.twimg.com/media/Co5LIYnXgAAL9HM?format=jpg&name=4096x4096")
+@raif = Person.create!(name: "Raif Badawi", picture: "https://pbs.twimg.com/media/D9Mek5gW4AIMiU1?format=jpg&name=900x900")
