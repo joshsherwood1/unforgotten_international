d647cec7dc4474711e9b967d6911fab650f906b1
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-11-27 23:53:00 -0700

    Update show action in hero controller to filter media main versus non main media types

--- app/controllers/hero_controller.rb
+++ app/controllers/hero_controller.rb
@@ -3,5 +3,6 @@ class HeroController <ApplicationController
   def show
     people = Person.all
     @person = people[Date.today.hash % people.size]
+    @main_medias = @person.medias.where(main2: true)
   end
 end
